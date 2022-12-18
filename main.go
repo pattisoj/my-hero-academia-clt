1ded8e96ce48d06c55ec254813cef2d85d5e151f
pattisoj <joshpattison23@icloud.com>
2022-12-18 11:34:43 +0000

    Map example

--- main.go
+++ main.go
@@ -5,18 +5,14 @@ import (
 )
 
 func main() {
-	i := 7
-	inc(&i)
-	fmt.Println(i)
-}
+	vertices := make(map[string]int)
 
-func inc(x *int) {
-	*x++
-}
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
 
-/* This example is looking at pointers. Without the * on line 13 (before the int)
-and without the & on line 9 this function on line thirteen receives a copy so running the file
-would return 7 - as it doesn't edit the original due to lack of return interaction. 
-By pointing to it's place in memory (with the * and &) we are able to make 'i' = 8. HOWEVER!
-It is important to note the additional * on line 14. Without this we increment the memory address 
-it is important that we de reference the pointer in the func */
+	delete(vertices, "square")
+
+	fmt.Println(vertices)
+	fmt.Println(vertices["dodecagon"])
+}
