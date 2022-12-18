6ac3b41e4ba25d9db1356abf3146246d72a3a1cb
pattisoj <joshpattison23@icloud.com>
2022-12-18 11:28:20 +0000

    Pointer interaction explained

--- main.go
+++ main.go
@@ -13,3 +13,10 @@ func main() {
 func inc(x *int) {
 	*x++
 }
+
+/* This example is looking at pointers. Without the * on line 13 (before the int)
+and without the & on line 9 this function on line thirteen receives a copy so running the file
+would return 7 - as it doesn't edit the original due to lack of return interaction. 
+By pointing to it's place in memory (with the * and &) we are able to make 'i' = 8. HOWEVER!
+It is important to note the additional * on line 14. Without this we increment the memory address 
+it is important that we de reference the pointer in the func */
