89510591310ef7354860eb7a814010928170905b
pattisoj <joshpattison23@icloud.com>
2022-12-29 14:13:28 +0000

    GET request working - printing JSON of Shoto Todoroki

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	
-}
+	resp, err := http.Get("https://myheroacademia-api.onrender.com/characters/5")
+	if err != nil {
+	   log.Fatalln(err)
+	}
+ 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+	   log.Fatalln(err)
+	}
+ 
+	sb := string(body)
+	log.Printf(sb)
+ }
