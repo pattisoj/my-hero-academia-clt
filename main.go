4ae772548723ccb0f5b7706aaa7438eb59c5020a
pattisoj <joshpattison23@icloud.com>
2022-12-30 13:47:19 +0000

    Random number generation working

--- main.go
+++ main.go
@@ -4,9 +4,19 @@ import (
 	"io/ioutil"
 	"fmt"
 	"net/http"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	min := 1
+  	max := 133
+
+	// NOTE: I had to set a seed here to ensure a random number is generated each time
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println(rand.Intn(max - min) + min)
+
 	id := 5
 	url := fmt.Sprintf("https://myheroacademia-api.onrender.com/characters/%d", id)
 
